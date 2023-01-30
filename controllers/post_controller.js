81309ebfbd981e79eaf8d83c83b537b6143fe5b1
ABHISHEK SHISHODIA <abhishekshishodia100@gmail.com>
2023-01-30 15:40:15 +0530

    fevicon changes

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,3 +1,6 @@
 module.exports.post = function(req,res){
-    res.end('<h3>this is the post page </h3>');
-}
+    // res.end('<h3>this is the post page </h3>');
+    return res.render('post',{
+        title:"post"
+    });
+}   
