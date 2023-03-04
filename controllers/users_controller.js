4df06e57f53e661da241b167872827954f28b527
ABHISHEK SHISHODIA <abhishekshishodia100@gmail.com>
2023-03-04 19:30:58 +0530

    deleting post and comments (authorised)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -61,7 +61,11 @@ module.exports.createSession = function(req, res){
 }
 
 module.exports.destroySession = function(req, res){
-    req.logout();
-
-    return res.redirect('/');
+    req.logout(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      });
+    
 }
