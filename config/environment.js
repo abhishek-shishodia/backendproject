815eacde9b0dbda0ac71eed2f680a2383042121b
ABHISHEK SHISHODIA <abhishekshishodia100@gmail.com>
2023-05-20 12:07:09 +0530

    minor changes

---
+++ config/environment.js
@@ -0,0 +1,17 @@
+
+
+const development = {
+    name:"development",
+    asset_path:'/assets',
+    session_cookie_key:'blahsomething',
+    db:'codeial_development'
+    
+}
+
+
+const production = {
+    name:"production"
+}
+
+
+module.exports=development;
