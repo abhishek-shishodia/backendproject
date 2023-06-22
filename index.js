eb175e8b3676ea5401eac2caa9e45de49350e01e
ABHISHEK SHISHODIA <abhishekshishodia100@gmail.com>
2023-06-22 15:47:52 +0530

    backend uploads

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
+// const chatServer = require('http').Server(app);
+// const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+// chatServer.listen(5000);
+// console.log('chat server is listening on port 5000');
 const path= require('path');
 
 app.use(sassMiddleware({
